4553c38a5d130fc4e8f5041833ceeb18fc81ff42
ashothov1 <33330301+ashothov1@users.noreply.github.com>
2018-05-18 01:09:59 +0300

    Add files via upload

---
+++ Lesson 4/9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int[,] matr = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+            int n = (int)(Math.Sqrt(matr.Length)) ;
+            int k = 0;
+            int i1 = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (matr[i1, j] != matr[0, 0])
+                {
+                    Console.WriteLine("Матрица не является произведением единичной матрицы на число.");
+                    Console.ReadLine();
+                    return;
+                }
+                i1++;
+            }
+            for (int i = 0; i < n; i++)
+                for (int j=0; j<n; j++)
+                    if (i!=j)
+                        if (matr[i,j]!=0)
+                        {
+                            Console.WriteLine("Матрица не является произведением единичной матрицы на число.");
+                            Console.ReadLine();
+                            return;
+                        }
+                Console.WriteLine("Матрица является произведением единичной матрицы на число.");
+            Console.ReadLine();
+        }
+    }
+}
